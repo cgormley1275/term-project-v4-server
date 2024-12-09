c7e1cd5255e70ea5feea13cddd45b85faa4672c5
Chris <cgormley1275@gmail.com>
2024-12-09 18:38:07 -0500

    added functionality to add new posts

--- onestop/posts/schema.js
+++ onestop/posts/schema.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "UserModel" },
     poster: { type: String, required: true },
-    photo: { type: Buffer, required: true },
+    photo: { type: Buffer },
     destinationCity: { type: String, required: true },
     destinationCountry: { type: String, required: true },
     caption: { type: String, required: true },
-    views: { type: Number, required: true },
-    likes: { type: Number, required: true },
+    views: { type: Number },
+    likes: { type: Number },
 },
     { collection: "posts" }
 );
